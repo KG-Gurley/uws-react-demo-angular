1d7d1503caebf56e1dd2eb1a94a99f7cde00a740
Kara Gurley <gengxun.k.gurley@gmail.com>
2018-03-09 11:33:09 -0600

    just testing

--- server/src/wsclient-test/client2.js
+++ server/src/wsclient-test/client2.js
@@ -5,7 +5,7 @@ const ws = new WebSocket('ws://localhost:3000');
 ws.on('open', () => {
     console.log('Successful connected to server');
 
-    ws.send('hello server from client2');
+    ws.send('hello server from update client2');
 
     ws.on('message', message => {
         console.log('Server says: ', message);
